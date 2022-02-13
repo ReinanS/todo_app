70394dedb37497d8ef5443c104f79aeece6eb766
reinans <reinandesouza01@gmail.com>
2022-02-12 21:54:27 -0300

    feat: add todo_form

---
+++ lib/widgets/todo_form_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class TodoFormWidget extends StatelessWidget {
+  final String title;
+  final String description;
+  final ValueChanged<String> onChangedTitle;
+  final ValueChanged<String> onChangedDescription;
+  final VoidCallback onSavedTodo;
+
+  const TodoFormWidget({
+    Key? key,
+    this.title = '',
+    this.description = '',
+    required this.onChangedTitle,
+    required this.onChangedDescription,
+    required this.onSavedTodo,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        _buildTitle(),
+        SizedBox(height: 8),
+        _buildDescription(),
+        SizedBox(height: 32),
+        _buildBottom(context),
+      ],
+    );
+  }
+
+  Widget _buildTitle() {
+    return TextFormField(
+      maxLines: 1,
+      initialValue: title,
+      onChanged: onChangedTitle,
+      validator: (title) {
+        if (title!.isEmpty) {
+          return 'The title cannot be empty';
+        }
+      },
+      decoration:
+          InputDecoration(border: UnderlineInputBorder(), labelText: 'Title'),
+    );
+  }
+
+  Widget _buildDescription() {
+    return TextFormField(
+      maxLines: 3,
+      initialValue: description,
+      onChanged: onChangedDescription,
+      decoration: InputDecoration(
+          border: UnderlineInputBorder(), labelText: 'Description'),
+    );
+  }
+
+  Widget _buildBottom(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all(Theme.of(context).primaryColor)),
+        onPressed: onSavedTodo,
+        child: Text(
+          'Save',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
