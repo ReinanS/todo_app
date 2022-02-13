72d36cdf2365c52653b327c2e2d1b542ea8949c4
reinans <reinandesouza01@gmail.com>
2022-02-13 17:14:37 -0300

    feat: implement StreamTransformer

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -6,5 +9,29 @@ class Utils {
     ScaffoldMessenger.of(context)
       ..removeCurrentSnackBar()
       ..showSnackBar(SnackBar(content: Text(text)));
+  }
+
+  static DateTime? toDateTime(Timestamp? value) {
+    if (value == null) return null;
+
+    return value.toDate();
+  }
+
+  static dynamic fromDateTimeToJson(DateTime? date) {
+    if (date == null) return null;
+
+    return date.toUtc();
+  }
+
+  static StreamTransformer<QuerySnapshot<Map<String, dynamic>>, List<T>>
+      transformer<T>(T Function(Map<String, dynamic> json) fromJson) {
+    return StreamTransformer<QuerySnapshot<Map<String, dynamic>>,
+            List<T>>.fromHandlers(
+        handleData: (QuerySnapshot data, EventSink<List<T>> sink) {
+      final snaps = data.docs.map((doc) => doc.data()).toList();
+      final objects = snaps.map((json) => fromJson(json as dynamic)).toList();
+
+      sink.add(objects);
+    });
   }
 }
