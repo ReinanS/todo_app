f3d420ede5d49448ead251052b3ae6a81fb8e786
reinans <reinandesouza01@gmail.com>
2022-02-13 17:16:37 -0300

    style: add App colors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Main Colors
+  static const Color primaryColor = Color(0xFF0D47A1);
+  static final Color primaryLightColor = Colors.lightBlue[50]!;
+  static const Color secondaryColor = Color(0xFF59A9DC);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color whiteMedium = Color(0xFFF5F5F5);
+  static const Color backgroundColor = Color(0xFFF1EFF1);
+
+  static const Color red = Color(0xFFA3020F);
+  static const Color black = Color(0xFF514766);
+  static const Color grey = Color(0xFF6f727a);
+  static const Color lightGrey = Color(0xFFEEEEEE);
+  static final Color grey100 = Colors.grey[100]!;
+  static const Color border = Color(0xFFE1E1E6);
+
+  // Cores Acessoria de imagem
+
+  // Azul
+  static const Color azulEscuro = Color(0xFF062E6A);
+  static const Color azulMedio = Color(0xFF59a9dc);
+  static const Color azulClaro = Color(0xFFAAD1ED);
+
+  // Cinza
+  static const Color cinzaEscuro = Color(0xFF85878b);
+  static const Color cinzaMedio = Color(0xFFc4c5c7);
+  static const Color cinzaclaro = Color(0xFFd8d9da);
+
+  //Tons Escuros
+  static const Color tomescuroum = Color(0xFF050f16);
+  static const Color tomescurodois = Color(0xFF081824);
+  static const Color tomescurotres = Color(0xFF0b202f);
+  static const Color tomescuroquatro = Color(0xFF1A3A50);
+}
